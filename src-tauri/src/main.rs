1df1316c7d036274f5a37805b0a3fc668b3cd964
unknown <phillip@devbell.net>
2024-08-08 15:33:16 -0500

    initial project

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,22 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn main() {
+    #[cfg(debug_assertions)]
+    let builder = tauri::Builder::default().plugin(devtools::init());
+    
+    #[cfg(not(debug_assertions))]
+    let builder = tauri::Builder::default();
+
+
+    builder
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
